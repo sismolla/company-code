8f01b750722d370dd2becb376191f0448d2ef8ca
sisay molla <msisay967@gmail.com>
2025-09-01 20:08:33 +0000

    pwa editor new one

--- Pharmacy/settings.py
+++ Pharmacy/settings.py
@@ -209,6 +209,9 @@ PWA_APP_DISPLAY = 'standalone'
 PWA_APP_SCOPE = '/'
 PWA_APP_ID = '/'
 PWA_APP_START_URL = '/'
+PWA_APP_ICONS = [ 
+    {"src": "/static/logo_192.png", "sizes": "192x192"}
+    ]
 PWA_APP_SCREENSHOTS = [
     {
         "src": "/static/phone_scren.png",
